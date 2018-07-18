92cfb2781ab07cfec2c711b899132d038007b184
Denis Atkesone <denis.atkesone@gmail.com>
2018-07-18 19:46:47 +0200

    [UP] create Home

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { List, Map } from 'immutable';
+import Voting from '../components/Voting';
 
-const Home = () => <div className="row">Welcome Home!</div>;
+const pair = List.of('Brazil', 'France');
+const tally = Map({ 'Brazil': 5, 'France': 4 });
+
+const Home = () => (
+  <div className="row">
+    <Voting pair={pair} tally={tally} />
+  </div>
+);
 
 export default connect(null, null)(Home);
