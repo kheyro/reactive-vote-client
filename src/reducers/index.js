23bcb04d39ea513f6111edee6ee17d5cc0b41668
Denis Atkesone <denis.atkesone@gmail.com>
2018-07-18 18:23:40 +0200

    [ADD] create reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,12 @@
-import { combineReducers } from 'redux';
+import { Map, fromJS, isImmutable } from 'immutable';
 
-const reducer = (state = {}) => state;
+const reducer = (state = Map(), action) => {
+  switch (action.type) {
+    case 'SET_STATE':
+      return !isImmutable(action.state) ? fromJS(action.state) : action.state;
+    default:
+      return state;
+  }
+};
 
-const rootReducer = combineReducers({
-  reducer,
-});
-
-export default rootReducer;
+export default reducer;
