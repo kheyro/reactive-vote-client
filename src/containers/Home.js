48a01edfe84f25c98b7f3135e41ade07c1b17948
Denis Atkesone <denis.atkesone@gmail.com>
2018-07-18 20:38:06 +0200

    [UP] remove dumb data

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,15 +1,10 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { List, Map } from 'immutable';
 import { VotingContainer } from '../components/Voting';
-
-const pair = List.of('Brazil', 'France');
-const tally = Map({ 'Brazil': 5, 'France': 4 });
 
 const Home = () => (
   <div className="row">
-    <VotingContainer pair={pair} tally={tally} />
+    <VotingContainer />
   </div>
 );
 
-export default connect(null, null)(Home);
+export default Home;
