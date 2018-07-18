0a036f7a231ddc290f0c8cb2e502ff7c13aada3a
Denis Atkesone <denis.atkesone@gmail.com>
2018-07-18 19:47:42 +0200

    [UP] handle SET_STATE action

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,10 @@
-import { Map, fromJS, isImmutable } from 'immutable';
+import { Map, fromJS } from 'immutable';
 
 const reducer = (state = Map(), action) => {
   switch (action.type) {
     case 'SET_STATE':
-      return !isImmutable(action.state) ? fromJS(action.state) : action.state;
+      // return !isImmutable(action.state) ? fromJS(action.state) : action.state;
+      return state.mergeDeep(fromJS(action.state));
     default:
       return state;
   }
