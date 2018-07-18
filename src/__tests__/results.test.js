761b0c924c52d4659bcc96b3f364bf90cb3e6336
Denis Atkesone <denis.atkesone@gmail.com>
2018-07-18 18:24:50 +0200

    [UP] update results tests:

--- src/__tests__/results.test.js
+++ src/__tests__/results.test.js
@@ -2,13 +2,23 @@ import React from 'react';
 import { List, Map } from 'immutable';
 import { mount } from 'enzyme';
 import Results from '../components/Results';
+import Winner from '../components/Winner';
+
+let wrapper;
+
+beforeEach(() => {
+  wrapper = mount(<Results />);
+});
+
+afterEach(() => {
+  wrapper.unmount();
+});
 
 describe('Results', () => {
   it('renders entries with vote counts or zero', () => {
     const pair = List.of('Brazil', 'France');
     const tally = Map({ Brazil: 5 });
-
-    const wrapper = mount(<Results pair={pair} tally={tally} />);
+    wrapper.setProps({ pair, tally });
 
     expect(wrapper.find('.entry').length).toEqual(2);
 
@@ -17,17 +27,24 @@ describe('Results', () => {
 
     expect(wrapper.find('.entry').at(1).text()).toContain('France');
     expect(wrapper.find('.entry').at(1).text()).toContain('0');
-
-    wrapper.unmount();
   });
 
   it('invokes the next callback when next button is clicked', () => {
     let nextInvoked = false;
     const next = () => (nextInvoked = true);
-    const wrapper = mount(<Results next={next} />);
-
+    wrapper.setProps({ next });
     wrapper.find('button').simulate('click');
     expect(nextInvoked).toBe(true);
-    wrapper.unmount();
+  });
+
+  it('renders the winner when there is one', () => {
+    wrapper.setProps({
+      pair: List.of('Brazil', 'France'),
+      tally: Map(),
+      winner: 'France',
+    });
+
+    expect(wrapper.find(Winner).length).toEqual(1);
+    expect(wrapper.render().text()).toContain('France');
   });
 });
