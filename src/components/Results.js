ad71ecde067ad4b7680ee189faede3825c5f4801
Denis Atkesone <denis.atkesone@gmail.com>
2018-07-18 14:29:24 +0200

    [UP] add vote count

---
+++ src/components/Results.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+class Results extends Component {
+  getPair = () => this.props.pair || [];
+
+  getVotes = entry => {
+    if (this.props.tally && this.props.tally.has(entry)) {
+      return this.props.tally.get(entry);
+    }
+    return 0;
+  };
+
+  render() {
+    return (
+      <div className="results">
+        {this.getPair().map(entry => (
+          <div key={entry} className="entry">
+            <h1>{entry}</h1>
+            <div className="voteCount">
+              {this.getVotes(entry)}
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Results;
