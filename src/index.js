e759648dcf6d3749b2988daa0ef979284f89565e
Denis Atkesone <denis.atkesone@gmail.com>
2018-07-20 14:27:35 +0200

    [UP] pass socket argument to middleware

--- src/index.js
+++ src/index.js
@@ -2,12 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { Router, Route } from 'react-router';
+import io from 'socket.io-client';
+import { setState } from './actions';
 import configureStore from '../config/store';
 import history from '../config/history';
 
 import App from './App';
 
-const store = configureStore();
+const socket = io(`${location.protocol}//${location.hostname}:5000`);
+socket.on('state', state => store.dispatch(setState(state)));
+
+const store = configureStore(undefined, socket);
 
 ReactDOM.render(
   <Provider store={store}>
