70db2f470df49c79f1136ebeaccfb92ea848c177
Denis Atkesone <denis.atkesone@gmail.com>
2018-07-20 14:27:13 +0200

    [ADD] create middleware to emit action to websocket

--- config/store.js
+++ config/store.js
@@ -1,9 +1,10 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
+import remoteAction from './middleware';
 import rootReducer from '../src/reducers/index';
 
-export default function configureStore(preloadedState) {
-  const middlewares = [thunk];
+export default function configureStore(preloadedState, socket) {
+  const middlewares = [thunk, remoteAction(socket)];
   const middlewareEnhancer = applyMiddleware(...middlewares);
 
   const enhancers = [middlewareEnhancer];
