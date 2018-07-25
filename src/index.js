9c4672dea219b97365ab7d4f4d39ab2a769299b8
Denis Atkesone <denis.atkesone@gmail.com>
2018-07-25 17:04:18 +0200

    [RF] remove custom history

--- src/index.js
+++ src/index.js
@@ -5,18 +5,17 @@ import { Router, Route } from 'react-router';
 import io from 'socket.io-client';
 import { setState } from './actions';
 import configureStore from '../config/store';
-import history from '../config/history';
 
 import App from './App';
 
 const socket = io(`${location.protocol}//${location.hostname}:5000`);
-socket.on('state', state => store.dispatch(setState(state)));
-
 const store = configureStore(undefined, socket);
+
+socket.on('state', state => store.dispatch(setState(state)));
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router history={history}>
+    <Router>
       <Route component={App} />
     </Router>
   </Provider>,
